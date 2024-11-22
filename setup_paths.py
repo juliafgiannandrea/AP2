c22c5991ae73d0db129cc7ff62e9a12d265a4d39
Júlia Félix <juliafgiannandrea@gmail.com>
2024-11-22 08:29:51 -0300

    final

--- setup_paths.py
+++ setup_paths.py
@@ -15,7 +15,6 @@ sys.path.append(str(BACK_DIR))
 
 print(sys.path)
 
-
 #cada diretório é convertido para uma string e adicionado ao sys path
 #permite que qualquer módulo ou pacote nesses diretórios seja importado de qualquer lugar no código do projeto. Isso facilita o uso de módulos internos do projeto sem precisar especificar caminhos relativos complexos.
 #facilita a organização do código em módulos separados. 
